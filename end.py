91309ca4e0328816dbb8f3c982a9de8370af242f
Donald Motswiri <rmotswiri023@student.wethinkcode.co.za>
2023-09-13 06:50:21 +0200

    added notes

--- end.py
+++ end.py
@@ -10,3 +10,55 @@ Passing the whitespace to the end parameter (end='') indicates that the end char
 print('Donald ', end='')
 
 print('is awesome')
+
+'''
+
+'''
+
+rows = 5
+
+i = 0
+i = 1
+
+print('*', end='')
+i=2
+print('')
+
+print('*', end='')
+print('*', end='')
+i = 3
+
+print('')
+
+print('*', end='')
+print('*', end='')
+print('*', end='')
+i = 4
+
+print('')
+
+print('*', end='')
+print('*', end='')
+print('*', end='')
+print('*', end='')
+i = 5
+
+print('')
+
+print('*', end='')
+print('*', end='')
+print('*', end='')
+print('*', end='')
+print('*', end='')
+
+print('')
+
+'''
+#outer loop
+for i in range (0, rows + 1):
+
+    for j in range(0, i):
+        print(i, end='')
+        
+    print('')
+'''
